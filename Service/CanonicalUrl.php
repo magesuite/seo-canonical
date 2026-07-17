6bf87e68bec2ac4b6321e3b1a4649c9fbd33afa8
Krzysztof Tomczyk <krzysztof.tomczyk@creativestyle.pl>
2026-07-17 08:22:31 +0000

    Feat: [RK-2311] Remove trailing slash from canonical

--- Service/CanonicalUrl.php
+++ Service/CanonicalUrl.php
@@ -59,7 +59,7 @@ class CanonicalUrl
             return $this->categoryHelper->getCanonicalUrl($url);
         }
 
-        if ($this->isHomepageWithStoreCodeInPath($url)) {
+        if (!$this->configuration->isRemovingTrailingSlashEnabled() && $this->isHomepageWithStoreCodeInPath($url)) {
             return $url;
         }
 
